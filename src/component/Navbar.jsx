d8ef540f05a8090f8ae885ebc2ac874cb22b3cf0
sagarpandey141 <121431268+sagarpandey141@users.noreply.github.com>
2024-03-01 13:34:57 +0530

    logo commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   return (
     <section className=''>
     <div className={`  ${styles.paddingX}  w-11/12 mx-auto flex justify-between  text-white `}>
-        <div className=' flex mt-3  gap-3'>
-            <img src={logo} className=' h-9 w-9' />
+        <div className=' flex justify-center items-center mt-3  gap-3'>
+            <img src={logo} className=' h-11 w-11' />
             <p className='text-white text-[18px] font-bold cursor-pointer flex '>Sagar | Full Stack Developer</p>
         </div>
         <div className=' flex  mt-3'>
             <ul className=' flex  gap-6'>
-                <a href='#about'>About</a>
+                <a href='#about' className=' font-bold text-orange-500'>About</a>
                 <a href='#work'>Work</a>
-                <a href='#contact'>Contact</a>
+                <a href='#contact' className=' font-bold text-green-500'>Contact</a>
             </ul>
         </div>
     </div>
